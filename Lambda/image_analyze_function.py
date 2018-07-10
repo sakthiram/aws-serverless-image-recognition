b19dc6b20363e396cd67c9f973da092350212dd0
ANT\sakthisi <sakthisi@amazon.com>
2018-07-10 11:41:17 -0700

    Using bucket & key from event parameter in lambda

--- Lambda/image_analyze_function.py
+++ Lambda/image_analyze_function.py
@@ -2,8 +2,8 @@ import json
 import boto3
 import os
 
-BUCKET = "test-walleye"
-KEY = "wallpaper.jpg"
+# BUCKET = "test-walleye"
+# KEY = "wallpaper.jpg"
 
 def lambda_handler(event, context):
     # body = {
@@ -12,6 +12,9 @@ def lambda_handler(event, context):
     # }
 
     # rekognition = boto3.client("rekognition", os.environ['AWS_REGION']) # us-west-1 rekognition service is not supported
+    BUCKET = event.get('Records')[0].get('s3').get('bucket').get('name')
+    KEY = event.get('Records')[0].get('s3').get('object').get('key')
+    
     rekognition = boto3.client("rekognition", "us-west-2")
     response = rekognition.detect_labels(
                 Image = {
